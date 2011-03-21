8e07fb5ae316881b0f6ba8b7840b1f82c56723a0
Bob Roberts <bob@ubuntu2.(none)>
2011-03-20 19:37:02 -0700

    added new projects

--- wsgi.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-os.environ['DJANGO_SETTINGS_MODULE'] = 'ramen.settings'
-import django.core.handlers.wsgi
-djangoapplication = django.core.handlers.wsgi.WSGIHandler()
-def application(environ, start_response):
-    if 'SCRIPT_NAME' in environ:
-        del environ['SCRIPT_NAME']
-    return djangoapplication(environ, start_response)
